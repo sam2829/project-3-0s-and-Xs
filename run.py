35978ed3fcb35b309f3837cf44bc18bfb0ae81e5
sam2829 <>
2023-08-26 15:18:49 +0000

    Add frame to the board game for which the game will be played on

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+game_board = ["-", "-", "-",
+"-", "-", "-",
+"-", "-", "-",]
+
+current_player = "x"
+winner = None
+
+def print_game_board(game_board):
+    """
+    This function is printing out the grid for which the game is played on
+    """
+    print("---------------")
+    print(" | " + game_board[0] + " | " + game_board[1] + " | " + game_board[2] + " | ")
+    print("---------------")
+    print(" | " + game_board[3] + " | " + game_board[4] + " | " + game_board[5] + " | ")
+    print("---------------")
+    print(" | " + game_board[6] + " | " + game_board[7] + " | " + game_board[8] + " | ")
+    print("---------------")
+
+print_game_board(game_board)

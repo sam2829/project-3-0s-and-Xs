b98ac35e75e787596b9db6bad2bb2ba30e5cc095
sam2829 <>
2023-08-27 09:32:50 +0000

    Add function to control player input

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 class PlayGame:
 
-    def __init__(self, game_board):
+    def __init__(self, game_board, current_player):
         """
         
         """
         self.game_board = game_board
+        self.current_player = current_player
 
-    def print_game_board(self, game_board):
+    def print_game_board(self, game_board, current_player):
         """
         This function is printing out the grid for which the game is played on
         """
@@ -19,6 +20,29 @@ class PlayGame:
         print("---------------")
         print(" | " + game_board[6] + " | " + game_board[7] + " | " + game_board[8] + " | ")
         print("---------------")
+
+    def player_input(self, game_board, current_player):
+        """
+        This function allows the user to select a grid position 1-9, the value entered must be an
+        integer and must be a value 1 - 9.
+        """
+        self.game_board = game_board
+        self.current_player = current_player
+
+        while True:
+
+            try:
+                inp = int(input("Please select a grid position 1 - 9:\n "))
+                if 1<= inp <=9 and self.game_board[inp-1] == "-":
+                    self.game_board[inp-1] = current_player
+                    break
+                
+                else:
+                    raise ValueError(f"Please select a value between numbers 1 - 9")
+            except ValueError as e:
+                print(f"Invalid number: {e}, please try again.\n")
+    
+           
 
 def new_game():
     """
@@ -38,8 +62,9 @@ def new_game():
     print(f"Welcome {player_name} lets get start the game!")
     print("-" * 45)
     game_running = True
-    game = PlayGame(game_board)
-    game.print_game_board(game_board)
+    game = PlayGame(game_board, current_player)
+    game.print_game_board(game_board, current_player)
+    game.player_input(game_board, current_player)
 
 
 new_game()

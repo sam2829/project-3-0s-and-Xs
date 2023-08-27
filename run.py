2d323de2cccdfdda4f5b522835415618a7877f8a
sam2829 <>
2023-08-27 10:38:16 +0000

    Add elif statement for if user has selected a value which has already been selected. Add style to print statements so console easier to read

--- run.py
+++ run.py
@@ -34,11 +34,17 @@ class PlayGame:
         while True:
 
             try:
+                print("-" * 45)
                 print(f"It's your turn {player_name}.\n")
+                print("-" * 45)
                 inp = int(input("Please select a grid position 1 - 9:\n "))
                 if 1<= inp <=9 and self.game_board[inp-1] == "-":
                     self.game_board[inp-1] = current_player
                     break
+                
+                elif self.game_board != "-":
+                    print("-" * 45)
+                    print(f"Looks like that spot is already taken, please try again.\n")
                 
                 else:
                     raise ValueError(f"Please select a value between numbers 1 - 9")
@@ -67,6 +73,11 @@ def new_game():
     game = PlayGame(game_board, current_player, game_running)
     game.print_game_board(game_board, current_player)
     game.player_input(game_board, current_player, player_name)
+    
+    #while game_running:
+       #game.print_game_board(game_board, current_player)
+       # game.player_input(game_board, current_player, player_name)
+
 
 
 new_game()
